d6a4a39c4a2dc120ece4168a05fd284a2c3a8ab4
Andreas Bielawski <brawl@nyanya.de>
2021-05-26 17:46:31 +0200

    next-page-transitions: Skip initial transitions

--- pages/_app.js
+++ pages/_app.js
@@ -37,6 +37,7 @@ function MyApp({ Component, pageProps }) {
       <AppNavbar />
 
       <PageTransition
+        skipInitialTransition
         timeout={TIMEOUT}
         classNames="page-transition"
         loadingComponent={<Loader />}
