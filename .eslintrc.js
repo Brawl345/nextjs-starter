7b29efdd0ad92a228c0f3e0cada991c05deeea0b
Andreas Bielawski <brawl@nyanya.de>
2021-06-17 21:45:02 +0200

    Reduce ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,5 @@
 module.exports = {
-  extends: [
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:react/recommended',
-    'next',
-    'prettier',
-  ],
-  plugins: ['react-hooks'],
+  extends: ['next', 'airbnb', 'airbnb/hooks', 'prettier'],
   settings: {
     'import/resolver': {
       node: {
@@ -25,70 +18,25 @@ module.exports = {
   },
   reportUnusedDisableDirectives: true,
   rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'jsx-a11y/click-events-have-key-events': 'off',
-    'jsx-a11y/anchor-is-valid': 'off',
-    'jsx-a11y/control-has-associated-label': [
-      2,
-      {
-        ignoreElements: ['option', 'th'],
-        depth: 3,
-      },
-    ],
-    'indent': 'off',
-    'no-underscore-dangle': 'off',
-    'linebreak-style': 'off',
-    'max-len': [
-      'warn',
-      {
-        code: 120,
-        ignoreTemplateLiterals: true,
-      },
-    ],
     'no-nested-ternary': 'off',
-    'no-trailing-spaces': 'off',
-    'no-param-reassign': [
-      'error',
-      {
-        props: false,
-      },
-    ],
-    'no-undef': 'off',
     'no-unused-expressions': [
       'error',
       {
         allowTernary: true,
       },
     ],
-    'object-curly-newline': [
-      'error',
+   "react/jsx-props-no-spreading"': [
+     "error"',
       {
-        ImportDeclaration: 'never',
-      },
+        html:"ignore"',
+        exceptions: "Component"',
+      ,
     ],
-    'quote-props': ['warn', 'consistent'],
-    'react/destructuring-assignment': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/jsx-indent': 'off',
-    'react/jsx-one-expression-per-line': 'off',
-    'react/jsx-props-no-spreading': [
-      'error',
-      {
-        html: 'ignore',
-        exceptions: ['Component'],
-      },
-    ],
-    'react/display-name': 'off',
-    'react/no-array-index-key': 'off',
-    'react/no-did-update-set-state': 'off',
-    'react/prop-types': 2,
-    'react/forbid-prop-types': 'off',
+   "react/no-array-index-key"':"off"',
+   "react/prop-types"': 1,
+   "react/forbid-prop-types"':"off",
   },
   globals: {
     React: 'writable',
-  },
-  env: {
-    amd: true,
   },
 };
