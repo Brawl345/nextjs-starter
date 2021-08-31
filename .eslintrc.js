649643188a2d335d1eb9a8ec30c8832b2a2e8e4e
Andreas Bielawski <brawl@nyanya.de>
2021-08-31 15:44:39 +0200

    Add ESLint Unicorn

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['next', 'airbnb', 'airbnb/hooks', 'prettier'],
+  plugins: ['unicorn'],
   settings: {
     'import/resolver': {
       node: {
@@ -18,18 +19,18 @@ module.exports = {
   },
   reportUnusedDisableDirectives: true,
   rules: {
-    "@next/next/no-img-element": "off",
-    "no-nested-ternary": "off",
-    "no-underscore-dangle": "off",
-    "no-unused-expressions": [
-      "error",
+    '@next/next/no-img-element': 'off',
+    'no-nested-ternary': 'off',
+    'no-underscore-dangle': 'off',
+    'no-unused-expressions': [
+      'error',
       {
-        allowTernary: true
-      }
+        allowTernary: true,
+      },
     ],
-    "react/no-array-index-key": "off",
-    "react/jsx-props-no-spreading": [
-      "error",
+    'react/no-array-index-key': 'off',
+    'react/jsx-props-no-spreading': [
+      'error',
       {
         html: 'ignore',
         exceptions: ['Component'],
@@ -37,12 +38,62 @@ module.exports = {
     ],
     'react/prop-types': 'error',
     'react/forbid-prop-types': 'off',
+    'unicorn/better-regex': 'error',
+    'unicorn/catch-error-name': 'error',
+    'unicorn/error-message': 'error',
+    'unicorn/escape-case': 'error',
+    'unicorn/new-for-builtins': 'error',
+    'unicorn/no-abusive-eslint-disable': 'error',
+    'unicorn/no-array-callback-reference': 'error',
+    'unicorn/no-array-method-this-argument': 'error',
+    'unicorn/no-array-push-push': 'error',
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/no-console-spaces': 'error',
+    'unicorn/no-document-cookie': 'error',
+    'unicorn/no-for-loop': 'error',
+    'unicorn/no-hex-escape': 'error',
+    'unicorn/no-instanceof-array': 'error',
+    'unicorn/no-lonely-if': 'error',
+    'unicorn/no-new-array': 'error',
+    'unicorn/no-new-buffer': 'error',
+    'unicorn/no-object-as-default-parameter': 'error',
+    'unicorn/no-process-exit': 'error',
+    'unicorn/no-static-only-class': 'error',
+    'unicorn/no-this-assignment': 'error',
+    'unicorn/no-useless-undefined': 'error',
+    'unicorn/numeric-separators-style': 'error',
+    'unicorn/prefer-array-find': 'error',
+    'unicorn/prefer-array-flat': 'error',
+    'unicorn/prefer-array-flat-map': 'error',
+    'unicorn/prefer-array-index-of': 'error',
+    'unicorn/prefer-array-some': 'error',
+    'unicorn/prefer-date-now': 'error',
+    'unicorn/prefer-default-parameters': 'error',
+    'unicorn/prefer-keyboard-event-key': 'error',
+    'unicorn/prefer-math-trunc': 'error',
+    'unicorn/prefer-negative-index': 'error',
+    'unicorn/prefer-number-properties': 'error',
+    'unicorn/prefer-reflect-apply': 'error',
+    'unicorn/prefer-regexp-test': 'error',
+    'unicorn/prefer-set-has': 'error',
+    'unicorn/prefer-spread': 'error',
+    'unicorn/prefer-string-replace-all': 'error',
+    'unicorn/prefer-string-slice': 'error',
+    'unicorn/prefer-string-starts-ends-with': 'error',
+    'unicorn/prefer-string-trim-start-end': 'error',
+    'unicorn/prefer-switch': 'error',
+    'unicorn/prefer-ternary': 'error',
+    'unicorn/prefer-type-error': 'error',
+    'unicorn/require-array-join-separator': 'error',
+    'unicorn/require-number-to-fixed-digits-argument': 'error',
+    'unicorn/throw-new-error': 'error',
   },
   globals: {
-    React: "writable"
+    React: 'writable',
   },
   env: {
     browser: true,
+    es6: true,
     node: true,
   },
 };
