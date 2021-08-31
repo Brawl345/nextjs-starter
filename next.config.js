75609e7fbd966785b6e99bc84e6aedd4d4e2966e
Andreas Bielawski <brawl@nyanya.de>
2021-08-31 15:53:55 +0200

    Setup Bundle Analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 module.exports = {
   eslint: {
     // Warning: Only enable this if you do linting through CI before pushing to production!
     ignoreDuringBuilds: true,
   },
-}
+};
+
+module.exports = withBundleAnalyzer({});
