11a57fbe4b1ad37c451444c0f814d39a77fc52cc
Andreas Bielawski <brawl@nyanya.de>
2021-09-03 14:38:41 +0200

    Add LICENSE, fix next.config.js and exclude yarn.lock from Prettier

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-module.exports = {
+const nextConfig = {
   eslint: {
     // Warning: Only enable this if you do linting through CI before pushing to production!
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = withBundleAnalyzer({});
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
